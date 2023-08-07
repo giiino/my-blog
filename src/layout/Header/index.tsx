d05e7d2396530ff17b64498512a3416bd9aa4e85
LAPTOP-803H1ARJ\user <ginobee33665@gmail.com>
2023-08-07 23:32:08 +0800

    feat: 更改網站icon

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,11 +1,7 @@
-import * as React from 'react'
-
 import styled from '@emotion/styled'
 import DarkModeIcon from '@mui/icons-material/DarkMode'
 import MenuIcon from '@mui/icons-material/Menu'
 import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
