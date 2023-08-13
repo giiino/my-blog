ecf72922bc33b6bb998dc09113f9bf034d9d1ac0
LAPTOP-803H1ARJ\user <ginobee33665@gmail.com>
2023-08-13 21:23:54 +0800

    chore: 補上未存到檔案

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -4,8 +4,8 @@ import { ThemeProvider, createTheme } from '@mui/material/styles'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-import { reactQueryConf } from '@/config/react-query.conf'
-import { themeConf } from '@/config/theme.conf'
+import { reactQueryConf } from '@/config/react-query'
+import { themeConf } from '@/config/theme'
 
 import GlobalStateProvider from './global-state.provider'
 
