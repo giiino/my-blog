2c811df4f679d4dff27f6a25b187ae561fb3b1d2
LAPTOP-803H1ARJ\user <ginobee33665@gmail.com>
2024-06-07 23:29:55 +0800

    chore: 修改sidemap 移除部分路徑

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL,
-  generateRobotsTxt: true
+  generateRobotsTxt: true,
+  exclude: ['/tool', '/edit', 'login'],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/tool', '/edit', 'login']
+      }
+    ]
+  }
 }
