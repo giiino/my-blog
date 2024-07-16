d09a83e54b1f95e711f2ed186ba79a9166111819
LAPTOP-803H1ARJ\user <ginobee33665@gmail.com>
2024-07-16 19:29:50 +0800

    feat: 修改letter space

--- src/shared/components/markdown/style-wrapper.ts
+++ src/shared/components/markdown/style-wrapper.ts
@@ -5,7 +5,7 @@ export const StyleWrapper = styled.div`
   .reacr-mark-down {
     > * {
       margin-bottom: 23px;
-      letter-spacing: 1.5px;
+      letter-spacing: 1.3px;
     }
     .paragraph {
       figure {
