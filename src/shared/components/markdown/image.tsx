b1235a614ef63693d89347715dadf39cd11c322a
LAPTOP-803H1ARJ\user <ginobee33665@gmail.com>
2024-07-18 00:50:51 +0800

    fix: 圖片尺寸錯誤

--- src/shared/components/markdown/image.tsx
+++ src/shared/components/markdown/image.tsx
@@ -22,8 +22,8 @@ export const Image = ({ alt, src }: Props) => {
       <ImageWrapper isover={ratio > 1 ? '1' : '0'}>
         <EnhancedImage
           src={src}
-          width={width * 5}
-          height={height * 5}
+          width={800}
+          height={1200}
           imageWidth='100%'
           ratio={ratio}
           alt={figcaptionValue || alt}
